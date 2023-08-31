bb6f9f10478db9b6ea5636048280e70913dd2359
shivamsherje <sherjes49@gmail.com>
2023-08-31 16:34:16 +0530

    commented some code in logging route now it will only save in data.txt

--- routes/Logging.route.js
+++ routes/Logging.route.js
@@ -11,17 +11,17 @@ LoggingRoute.post('/post', async (req, res) => {
         const data = new LoggingModel(req.body);
 
         // Save the data to the database
-        const savedData = await data.save();
+        // const savedData = await data.save();
 
         // Fetch the saved data from the database by its _id
-        const retrievedData = await LoggingModel.findById(savedData._id);
+        // const retrievedData = await LoggingModel.findById(savedData._id);
 
-        console.log('Retrieved data:', retrievedData.toObject());
+        // console.log('Retrieved data:', retrievedData.toObject());
 
 
         // Create a text file and append data to it
         const filePath = 'data.txt';
-        const fileData = JSON.stringify(retrievedData, null, 2); // Convert data to nicely formatted JSON
+        const fileData = JSON.stringify(data.toObject(), null, 2);  // Convert data to nicely formatted JSON
 
         fs.appendFileSync(filePath, fileData + '\n'); // Append data with a newline separator
 
